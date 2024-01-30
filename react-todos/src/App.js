b8de607a14b18961780191e3e79944524e0a0d16
Vinay Chowdry <vinaychowdry31@gmail.com>
2024-01-29 19:36:50 -0500

    React: Creating a button to add new todos by using onClick and usage of hooks(useState) and destructuring

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,14 +1,26 @@
+import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable.js';
 
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState([
     { rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
     { rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
     { rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User Three'},
     { rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One'}
-  ]
+  ])
+
+  const addTodo = () =>{
+    if ( todos.length > 0){
+      const newTodo ={
+        rowNumber:todos.length + 1,
+        rowDescription: 'New Todo',
+        rowAssigned: 'User Three'
+      };
+      setTodos( todos => [...todos, newTodo]);
+    }
+  }
 
   return (
     <div className="mt-5 container">
@@ -18,6 +30,9 @@ function App() {
         </div>
         <div className="card-body">
           <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div>
