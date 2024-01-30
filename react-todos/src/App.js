c74b22fc2dac65d63a18e009733b832f3eecd658
Vinay Chowdry <vinaychowdry31@gmail.com>
2024-01-29 20:41:38 -0500

    React: Adding keys for todo rows and deleting todos

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable.js';
 import NewTodoForm from './components/NewTodoForm.js';
@@ -6,21 +6,33 @@ import NewTodoForm from './components/NewTodoForm.js';
 function App() {
 
   const [todos, setTodos] = useState([
-    { rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
-    { rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
-    { rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User Three'},
-    { rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One'}
+    { rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One' },
+    { rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two' },
+    { rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User Three' },
+    { rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One' }
   ])
 
-  const addTodo = (description,assigned) =>{
-    if ( todos.length > 0){
-      const newTodo ={
-        rowNumber:todos.length + 1,
-        rowDescription: description,
-        rowAssigned: assigned
-      };
-      setTodos( todos => [...todos, newTodo]);
+  const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
     }
+    else {
+      rowNumber = 1;
+    }
+    const newTodo = {
+      rowNumber: rowNumber,
+      rowDescription: description,
+      rowAssigned: assigned
+    };
+    setTodos(todos => [...todos, newTodo]);
+  }
+
+  const deleteTodo = (deleteTodoRowNumber) =>{
+    let filtered = todos.filter(function(value){
+      return value.rowNumber!==deleteTodoRowNumber;
+    });
+    setTodos(filtered);
   }
 
   return (
@@ -30,11 +42,11 @@ function App() {
           Your Todo's
         </div>
         <div className="card-body">
-          <TodoTable todos={todos}/>
-          <button className='btn btn-primary' onClick={addTodo}>
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button className='btn btn-primary'>
             Add new todo
           </button>
-          <NewTodoForm addTodo={addTodo}/>
+          <NewTodoForm addTodo={addTodo} />
         </div>
       </div>
     </div>
